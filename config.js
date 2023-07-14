87f972e92b6b5b3440dafcf56c5b659a0a5c223f
Misha Kukarkin <wardenclock@gmail.com>
2023-07-14 18:29:21 +0500

    init commit

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+  TWITCH_SECRET: process.env.TWITCH_SECRET,
+  TWICH_TOKEN: process.env.TWICH_TOKEN,
+};
